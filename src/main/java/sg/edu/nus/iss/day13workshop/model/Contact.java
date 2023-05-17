ce3848109e8757ad63a732f1aa898124d618c7e5
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-05-17 14:42:57 +0800

    added extra fields and methods to Contact class

--- src/main/java/sg/edu/nus/iss/day13workshop/model/Contact.java
+++ src/main/java/sg/edu/nus/iss/day13workshop/model/Contact.java
@@ -1,8 +1,17 @@
 package sg.edu.nus.iss.day13workshop.model;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Random;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class Contact {
@@ -18,14 +27,42 @@ public class Contact {
     @Size(min=8, message = "Invalid phone number")
     private String phoneNumber;
 
+    @NotNull(message = "Date of birth is mandatory")
+    @Past(message = "Date of birth should not be from the future!")
+    @DateTimeFormat(pattern = "MM-dd-yyyy")
+    private LocalDate dateOfBirth;
+
+    @Min(value = 10, message = "Must be above 10 years old")
+    @Max(value = 100, message = "Must be below 100 years old")
+    private int age;
+
+    private String id;
+
     public Contact() {
+        this.id = generateID(8);
     }
 
-    public Contact(String name, String email, String phoneNumber) {
+    private String generateID(int i) {
+        Random r = new Random();
+        StringBuffer sb = new StringBuffer();
+
+        while (sb.length() < i) {
+            sb.append(Integer.toHexString(r.nextInt(i)));
+        }
+
+        return sb.toString().substring(0, i);
+    }
+
+    public Contact(String name, String email, String phoneNumber, LocalDate dateofBirth, int age, String id) {
+
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
-    }
+        this.dateOfBirth = dateofBirth;
+        this.age = age;
+        this.id = id;
+
+    }    
 
     public String getName() {
         return name;
@@ -51,10 +88,40 @@ public class Contact {
         this.phoneNumber = phoneNumber;
     }
 
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        // calculate the age
+        int calculatedAge = 0;
+
+        if (dateOfBirth != null) {
+            calculatedAge = Period.between(dateOfBirth, LocalDate.now()).getYears();
+        }
+
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "Contact [name=" + name + ", email=" + email + ", phoneNumber=" + phoneNumber + "]";
     }
-
 
 }
